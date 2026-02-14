ef8b8fa102686a5a801728d1180175f7ce2cc377
khaled <k_wahby@hotmail.com>
2026-02-14 12:47:03 +0200

    HW_Lec8

--- HW_Lec8/leetcode.dart
+++ HW_Lec8/leetcode.dart
@@ -12,4 +12,16 @@ Constraints
 1 <= s.length, t.length <= 5 * 104
 s and t consist of lowercase English letters.
   */
+
+  String s = "anagram";
+  String t = "nagaram";
+
+  List<String> listS = s.split("");
+  List<String> listT = t.split("");
+  listS.sort();
+  listT.sort();
+  s = listS.toString();
+  t = listT.toString();
+
+  print(s == t);
 }
