b29d1a8268bde564c0f35c659b5b876d6f690f28
khaled <k_wahby@hotmail.com>
2026-03-16 23:13:57 +0200

    HW_Lec12

--- hw_lec12/lib/main.dart
+++ hw_lec12/lib/main.dart
@@ -1,122 +1,242 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row1(),
+            SizedBox(height: 20),
+            Row2(),
+            SizedBox(height: 20),
+            Row3(),
+            SizedBox(height: 20),
+            Row4(),
+            SizedBox(height: 20),
+            Row5(),
+            SizedBox(height: 20),
+            Row6(),
+            SizedBox(height: 20),
+            Row7(),
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// ============== R O W 1 ============================
+class Row1 extends StatelessWidget {
+  const Row1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Container(
+      width: double.infinity, // full width of Column
+      height: 160,
+      color: const Color.fromARGB(255, 228, 242, 253),
+    );
+  }
+}
+
+// ============== R O W 2 ============================
+class Row2 extends StatelessWidget {
+  const Row2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          width: 30,
+          height: 20,
+          color: Color.fromARGB(255, 224, 224, 224),
         ),
+        SizedBox(width: 10),
+        Container(
+          width: 350,
+          height: 20,
+          color: Color.fromARGB(255, 224, 224, 224),
+          alignment: Alignment.centerLeft,
+        ),
+      ],
+    );
+  }
+}
+
+// ============== R O W 3 ============================
+class Row3 extends StatelessWidget {
+  const Row3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 3,
+      color: Color.fromARGB(255, 224, 224, 224),
+    );
+  }
+}
+
+// ============== R O W 4 ============================
+
+class Row4 extends StatelessWidget {
+  const Row4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          width: 180,
+          height: 100,
+          color: const Color.fromARGB(255, 230, 247, 231),
+          child: Column(
+            children: [
+              Container(
+                width: 190,
+                height: 45,
+                color: const Color.fromARGB(255, 165, 214, 167),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 190,
+                height: 45,
+                color: const Color.fromARGB(255, 165, 214, 167),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 20),
+        Container(
+          width: 190,
+          height: 100,
+          color: const Color.fromARGB(255, 255, 242, 224),
+          alignment: Alignment.centerLeft,
+          child: Row(
+            children: [
+              Container(
+                width: 90,
+                height: 100,
+                color: const Color.fromARGB(255, 255, 204, 128),
+              ),
+              SizedBox(width: 10),
+              Container(
+                width: 90,
+                height: 100,
+                color: const Color.fromARGB(255, 255, 204, 128),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// ============== R O W 5 ============================
+class Row5 extends StatelessWidget {
+  const Row5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 390,
+      color: const Color.fromARGB(255, 243, 229, 243),
+      child: Row(
+        children: [
+          Container(
+            height: 100,
+            width: 92.5,
+            color: const Color.fromARGB(255, 225, 190, 232),
+          ),
+          SizedBox(width: 10, height: 100),
+          Column(
+            children: [
+              Container(
+                height: 45,
+                width: 92.5,
+                color: const Color.fromARGB(255, 207, 147, 217),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 45,
+                width: 92.5,
+                color: const Color.fromARGB(255, 207, 147, 217),
+              ),
+            ],
+          ),
+          SizedBox(width: 10, height: 100),
+          Container(
+            height: 100,
+            width: 92.5,
+            color: const Color.fromARGB(255, 225, 190, 232),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+}
+
+// ============== R O W 6 ============================
+
+class Row6 extends StatelessWidget {
+  const Row6({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          width: 185,
+          height: 60,
+          color: const Color.fromARGB(255, 178, 223, 220),
+        ),
+        SizedBox(width: 20),
+        Container(
+          width: 185,
+          height: 60,
+          color: const Color.fromARGB(255, 128, 203, 196),
+        ),
+      ],
+    );
+  }
+}
+
+// ============== R O W 7 ============================
+
+class Row7 extends StatelessWidget {
+  const Row7({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity, // full width of Column
+      height: 40,
+      color: const Color.fromARGB(255, 224, 224, 224),
     );
   }
 }
